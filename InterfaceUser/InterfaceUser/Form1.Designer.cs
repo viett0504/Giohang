cd90fc2f6588d2f94218e13da827296344cb05b0
viett0504 <honmatroi@gmail.com>
2024-10-31 18:44:04 +0700

    Add files via upload

---
+++ InterfaceUser/InterfaceUser/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace InterfaceUser
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Label label1;
+            dataGridView1 = new DataGridView();
+            img = new DataGridViewTextBoxColumn();
+            name = new DataGridViewTextBoxColumn();
+            quantity = new DataGridViewTextBoxColumn();
+            price = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            dataGridView2 = new DataGridView();
+            dataGridViewTextBoxColumn1 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn2 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn3 = new DataGridViewTextBoxColumn();
+            dataGridViewTextBoxColumn4 = new DataGridViewTextBoxColumn();
+            label2 = new Label();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(133, 750);
+            label1.Name = "label1";
+            label1.Size = new Size(257, 65);
+            label1.TabIndex = 6;
+            label1.Text = "GIO HANG";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = SystemColors.ButtonFace;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { img, name, quantity, price });
+            dataGridView1.GridColor = Color.Black;
+            dataGridView1.Location = new Point(133, 171);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            dataGridView1.Size = new Size(1643, 540);
+            dataGridView1.TabIndex = 1;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // img
+            // 
+            img.HeaderText = "Image";
+            img.MinimumWidth = 10;
+            img.Name = "img";
+            img.Width = 390;
+            // 
+            // name
+            // 
+            name.HeaderText = "Name";
+            name.MinimumWidth = 10;
+            name.Name = "name";
+            name.Width = 390;
+            // 
+            // quantity
+            // 
+            quantity.HeaderText = "Quantity";
+            quantity.MinimumWidth = 10;
+            quantity.Name = "quantity";
+            quantity.Width = 390;
+            // 
+            // price
+            // 
+            price.HeaderText = "Price";
+            price.MinimumWidth = 10;
+            price.Name = "price";
+            price.Width = 390;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(1972, 255);
+            button1.Name = "button1";
+            button1.Size = new Size(334, 64);
+            button1.TabIndex = 2;
+            button1.Text = "Them vao gio hang";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(1972, 610);
+            button2.Name = "button2";
+            button2.Size = new Size(334, 64);
+            button2.TabIndex = 3;
+            button2.Text = "Xoa khoi gio hang";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button3.Location = new Point(1972, 984);
+            button3.Name = "button3";
+            button3.Size = new Size(334, 68);
+            button3.TabIndex = 4;
+            button3.Text = "Thanh toan";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.BackgroundColor = SystemColors.ButtonFace;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { dataGridViewTextBoxColumn1, dataGridViewTextBoxColumn2, dataGridViewTextBoxColumn3, dataGridViewTextBoxColumn4 });
+            dataGridView2.GridColor = Color.Black;
+            dataGridView2.Location = new Point(133, 833);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 82;
+            dataGridView2.Size = new Size(1643, 440);
+            dataGridView2.TabIndex = 5;
+            dataGridView2.CellContentClick += dataGridView2_CellContentClick;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            dataGridViewTextBoxColumn1.HeaderText = "Image";
+            dataGridViewTextBoxColumn1.MinimumWidth = 10;
+            dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            dataGridViewTextBoxColumn1.Width = 390;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            dataGridViewTextBoxColumn2.HeaderText = "Name";
+            dataGridViewTextBoxColumn2.MinimumWidth = 10;
+            dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            dataGridViewTextBoxColumn2.Width = 390;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            dataGridViewTextBoxColumn3.HeaderText = "Quantity";
+            dataGridViewTextBoxColumn3.MinimumWidth = 10;
+            dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            dataGridViewTextBoxColumn3.Width = 390;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            dataGridViewTextBoxColumn4.HeaderText = "Price";
+            dataGridViewTextBoxColumn4.MinimumWidth = 10;
+            dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            dataGridViewTextBoxColumn4.Width = 390;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 19.875F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(133, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(297, 71);
+            label2.TabIndex = 7;
+            label2.Text = "SAN PHAM";
+            label2.Click += label2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(2478, 1336);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView2);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private DataGridViewTextBoxColumn img;
+        private DataGridViewTextBoxColumn name;
+        private DataGridViewTextBoxColumn quantity;
+        private DataGridViewTextBoxColumn price;
+        private DataGridView dataGridView2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private Label label2;
+    }
+}
---
+++ InterfaceUser/InterfaceUser/Form1.cs
@@ -0,0 +1,131 @@
+﻿using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace InterfaceUser
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            LoadProductList();
+ 
+
+        }
+        List<Product> productList = new List<Product>();
+        List<Product> cart = new List<Product>();
+        private void LoadProductList()
+        {
+
+            productList.Add(new Product("C:\\Path\\To\\Image1.jpg", "Giày Nam", 5, 1000000));
+            productList.Add(new Product("C:\\Path\\To\\Image2.jpg", "Áo Thun", 3, 500000));
+
+            foreach (var product in productList)
+            {
+                dataGridView1.Rows.Add(product.Img, product.Name, product.Quantity, product.Price);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int index = dataGridView1.CurrentRow.Index;
+
+                if (index >= 0 && index < productList.Count)
+                {
+                    Product selectedProduct = productList[index];
+
+                    var existingProduct = cart.FirstOrDefault(p => p.Name == selectedProduct.Name);
+
+                    if (existingProduct != null)
+                    {
+                        existingProduct.Quantity += selectedProduct.Quantity;
+                        existingProduct.Price += selectedProduct.Price * (decimal)selectedProduct.Quantity;
+                    }
+                    else
+                    {
+                        cart.Add(new Product(selectedProduct.Img, selectedProduct.Name, selectedProduct.Quantity, selectedProduct.Price * (decimal)selectedProduct.Quantity));
+                    }
+                    UpdateCartView();
+                }
+                else
+                {
+                    MessageBox.Show("Sản phẩm không hợp lệ.", "Lỗi");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm để thêm vào giỏ hàng.", "Thông báo");
+            }
+        }
+        private void UpdateCartView()
+        {
+            dataGridView2.Rows.Clear();
+            foreach (var item in cart)
+            {
+                dataGridView2.Rows.Add(item.Img, item.Name, item.Quantity, item.Price);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView2.CurrentRow != null)
+            {
+                int index = dataGridView2.CurrentRow.Index;
+
+                if (index >= 0 && index < cart.Count)
+                {
+                    cart.RemoveAt(index);
+
+                    UpdateCartView();
+                }
+                else
+                {
+                    MessageBox.Show("Sản phẩm không hợp lệ.", "Lỗi");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm để xóa khỏi giỏ hàng.", "Thông báo");
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanh toan thanh cong");
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    public class Product
+    {
+        public String Img { get; set; }
+        public String Name { get; set; }
+        public double Quantity { get; set; }
+        public decimal Price { get; set; }
+        public Product(string img, string name, double quantity, decimal price ) { 
+            Img = img;
+            Name = name;
+            Quantity = quantity;
+            Price = price;
+        }
+ 
+    }
+}
